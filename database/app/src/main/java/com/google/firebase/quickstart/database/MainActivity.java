f3c3c6bf08880149ad9c6a14ce4292afe882663d
AlexisVincent <avincent32@gatech.edu>
2018-09-04 19:35:29 -0400

    Google Maps should be implemented!

--- database/app/src/main/java/com/google/firebase/quickstart/database/MainActivity.java
+++ database/app/src/main/java/com/google/firebase/quickstart/database/MainActivity.java
@@ -45,24 +45,27 @@ public class  MainActivity extends BaseActivity {
 
         // Create the adapter that will return a fragment for each section
         mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-            private final Fragment[] mFragments = new Fragment[] {
+            private final Fragment[] mFragments = new Fragment[]{
                     new RecentPostsFragment(),
                     new MyPostsFragment(),
                     new MyTopPostsFragment(),
             };
-            private final String[] mFragmentNames = new String[] {
+            private final String[] mFragmentNames = new String[]{
                     getString(R.string.heading_recent),
                     getString(R.string.heading_my_posts),
                     getString(R.string.heading_my_top_posts)
             };
+
             @Override
             public Fragment getItem(int position) {
                 return mFragments[position];
             }
+
             @Override
             public int getCount() {
                 return mFragments.length;
             }
+
             @Override
             public CharSequence getPageTitle(int position) {
                 return mFragmentNames[position];
@@ -79,6 +82,14 @@ public class  MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, NewPostActivity.class));
+            }
+        });
+
+        //button launches MapActivity
+        findViewById(R.id.buttonMap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity2.class));
             }
         });
     }
